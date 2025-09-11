fb9068374ae09b0341bfae5677df46c10489d2c6
Besarta Jonuzi <s2307401@student.hb.se>
2025-09-11 19:28:08 +0200

    Design och pagekontroll

--- Index.js
+++ Index.js
@@ -1,10 +1,12 @@
-    function showPage(pageId) {
+    function showPage(pageId) 
+    {
       // Hide all pages
       var pages = document.querySelectorAll('.page');
-      pages.forEach(function(page) {
+      pages.forEach(function(page) 
+      {
         page.classList.remove('active');
       });
 
       // Show the selected page
       document.getElementById(pageId).classList.add('active');
-      }
+    }
